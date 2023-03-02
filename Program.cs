ccb11075a3302d2ec5406615175bc62ad4a51ea4
Ben Hoberg <benjhoberg@gmail.com>
2023-03-02 15:44:30 -0600

    phase 1 complete

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace hobergHeist
+namespace Heist
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan your heist.");
+            Console.WriteLine();
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your skill level?");
+            int skill = int.Parse(Console.ReadLine());
+            Console.WriteLine("On a scale of 0.0 to 2.0, how courageous does your mom say you are?");
+            double courage = Convert.ToDouble(Console.ReadLine());
+
+            TeamMember newMember = new TeamMember(name, skill, courage);
+
+            Console.WriteLine($"{newMember.Name} has a skill level of {newMember.SkillLevel} and, on a scale of 1 to 2, they're {newMember.CourageLevel} nervous");
         }
     }
 }
---
+++ TeamMember.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Heist
+{
+    public class TeamMember
+    {
+        public string Name { get; set; }
+
+        public int SkillLevel { get; set; }
+
+        public double CourageLevel { get; set; }
+
+        public TeamMember(string name, int skillLevel, double courageLevel)
+        {
+            Name = name;
+            SkillLevel = skillLevel;
+            CourageLevel = courageLevel;
+        }
+    }
+}
