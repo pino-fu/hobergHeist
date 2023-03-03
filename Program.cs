91040f0367f9dc449d0864ef4c7ec27d072d288c
Ben Hoberg <benjhoberg@gmail.com>
2023-03-03 12:05:20 -0600

    phases 5 and 6 complete

--- Program.cs
+++ Program.cs
@@ -7,14 +7,9 @@ namespace Heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Plan your heist");
+            Console.Write("Plan your heist! Please enter a difficulty level (0-100): ");
+            int userDifficultyLevel = int.Parse(Console.ReadLine());
 
-            Bank targetBank = new Bank(100, "Gringott's");
-            int bankDifficulty = targetBank.difficultyLevel;
-            Random r = new Random();
-            int luck = r.Next(-10, 11);
-            int bankDifficultyWithLuck = bankDifficulty + luck;
-            
             string name = "Test";
             Dictionary<string, TeamMember> Team = new Dictionary<string, TeamMember>();
 
@@ -42,18 +37,37 @@ namespace Heist
                 Team.Add($"{newMember.Name}", newMember);
             }
 
-            Console.WriteLine($"Team Count: {Team.Count}");
-            Console.WriteLine($"Team Skill: {skillTotal}");
-            Console.WriteLine($"Bank Diffulty: {bankDifficultyWithLuck}");
+            Console.Write("How many simulations would you like to run? ");
+            int runCount = int.Parse(Console.ReadLine());
+            int can = 0;
+            int cant = 0;
 
-            if (targetBank.difficultyLevel > skillTotal)
+            for (int i = 0; i < runCount; i++)
             {
-                Console.WriteLine("It looks like your team doesn't have what it takes for this job.");
+                Bank targetBank = new Bank(userDifficultyLevel, "Gringott's");
+                int bankDifficulty = targetBank.difficultyLevel;
+                Random r = new Random();
+                int luck = r.Next(-10, 11);
+                int bankDifficultyWithLuck = bankDifficulty + luck;
+
+                Console.WriteLine($"Team Count: {Team.Count}");
+                Console.WriteLine($"Team Skill: {skillTotal}");
+                Console.WriteLine($"Bank Diffulty: {bankDifficultyWithLuck}");
+
+                if (targetBank.difficultyLevel > skillTotal)
+                {
+                    Console.WriteLine("It looks like your team doesn't have what it takes for this job.");
+                    Console.WriteLine();
+                    cant++;
+                }
+                else
+                {
+                    Console.WriteLine($"Congratulations, it looks like your team can handle {targetBank.Name}");
+                    Console.WriteLine();
+                    can++;
+                }
             }
-            else
-            {
-                Console.WriteLine($"Congratulations, it looks like your team can handle {targetBank.Name}");
-            }
+            Console.WriteLine($"Simulation Report: Successes: {can}, Failures: {cant}");
         }
     }
 }
